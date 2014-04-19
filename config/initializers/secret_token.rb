21aff40c76dff56376031f6b317ace5e2c9fa596
liu.hw@yougou.com <liu.hw@yougou.com>
2014-04-19 13:01:15 +0800

    create admin-templates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminTemplates::Application.config.secret_token = '6811fb2aa283969cb2e22ddbe6fae0eca0158ed88896b44c1c677a82877ccda63209fb55d39169def393d39c4f36d4f127b11a3dde458a48f41e0e7314b87072'
